77171ee5bd07a460b7fe9744f1a684ee693a1a17
threehundo <samuelbaik87@gmail.com>
2020-12-21 15:14:52 -0800

    Created state in App

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -3,9 +3,32 @@ import React from "react";
 import Heading from "./components/Heading";
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: null,
+      lastName: null,
+      cell: null,
+      email: null,
+      linkedIn: null,
+      workExperience: [
+        {
+          date: null,
+          company: null,
+          position: null,
+          summary: null,
+        },
+      ],
+      education: [
+        {
+          date: null,
+          school: null,
+          degree: null,
+        },
+      ],
+      skills: [],
+    };
+  }
   render() {
     return <Heading />;
   }
