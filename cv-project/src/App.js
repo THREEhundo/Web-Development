b27b51dbc4f849efd27cd32c65ad8cbfaba4a441
threehundo <samuelbaik87@gmail.com>
2020-12-21 14:50:57 -0800

    CV-project template

---
+++ cv-project/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import Heading from "./components/Heading";
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    return <Heading />;
+  }
+}
+
+export default App;
---
+++ cv-project/src/components/Heading.js
@@ -0,0 +1,12 @@
+function Heading() {
+  return (
+    <div>
+      <h1>CV-Template</h1>
+      <h2>
+        <a href="">Github</a>
+      </h2>
+    </div>
+  );
+}
+
+export default Heading;
