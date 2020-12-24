2cf85c829ebb2092b6cfc5409c11a6553a02c5e8
threehundo <samuelbaik87@gmail.com>
2020-12-23 21:08:08 -0800

    added temp & calc component

--- cv-project/src/components/GeneralInfo.js
+++ cv-project/src/components/GeneralInfo.js
@@ -1,7 +1,53 @@
+import "../App.css";
 import React from "react";
 
 class GeneralInfo extends React.Component {
   // Contact Info
+  render() {
+    const { nameClicked, handleNameClicked } = this.props;
+    let input;
+
+    // nameClicked ? (input = <NameInputs />) : (input = <div>Not clicked</div>);
+
+    if (nameClicked) {
+      input = <NameInputs />;
+    } else {
+      input = <h1> Name Clicked is false</h1>;
+    }
+
+    return (
+      <div>
+        {/* div & edit btn -> name input & check btn & x btn  -> div */}
+        <div>
+          Your Name
+          <input
+            type="image"
+            alt="edit-first-name"
+            src="./edit.png"
+            className="edit-btn"
+            onClick={handleNameClicked}
+          ></input>
+          {input}
+        </div>
+
+        {/* <label>
+          First Name
+          <input type="text" onChange={this.props.editFirstName}></input>
+        </label> */}
+      </div>
+    );
+  }
+}
+
+function NameInputs(props) {
+  // const { editFirstName, editLastName } = this.props;
+  return (
+    <div>
+      {/* <input onChange={editFirstName}></input>
+      <input onChange={editLastName}></input> */}
+      <h1>Working</h1>
+    </div>
+  );
 }
 
 export default GeneralInfo;
