accb5b1b36cf82792752c8d978137237b1adb953
threehundo <samuelbaik87@gmail.com>
2021-01-07 12:54:36 -0800

    Stateless functional components

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -3,15 +3,61 @@ import Navbar from "./components/navbar";
 import Counters from "./components/counters";
 import "./App.css";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 2 },
+      { id: 3, value: 0 },
+      { id: 4, value: 1 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({
+      counters,
+    });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({
+      counters,
+    });
+  };
+
+  handleDelete = (counterID) => {
+    console.log("Event Handler Called", counterID);
+    const counters = this.state.counters.filter((c) => c.id !== counterID);
+    this.setState({
+      counters,
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onReset={this.handleReset}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
